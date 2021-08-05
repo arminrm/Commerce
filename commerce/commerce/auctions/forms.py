b498c93aabf5c60b3badb81ec452c2da44f6ed71
Armin Raghem-Moayed <68967290+arminrm@users.noreply.github.com>
2021-08-05 09:39:43 -0400

    Delete forms.py

--- commerce/commerce/auctions/forms.py
+++
@@ -1,7 +0,0 @@
-from .models import Listing
-from django.forms import ModelForm
-
-class ListingForm(ModelForm):
-    class Meta:
-        model = Listing
-        fields = ['title', 'description', 'current_price']
